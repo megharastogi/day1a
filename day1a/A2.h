c31224a2b6edaee720d1df45cb1d298bd557cf6c
Megha Gulati <meghagulati30@gmail.com>
2013-09-09 21:53:42 -0700

    first commit

---
+++ day1a/A2.h
@@ -0,0 +1,13 @@
+//
+//  A2.h
+//  day1a
+//
+//  Created by MEGHA GULATI on 9/9/13.
+//  Copyright (c) 2013 MEGHA GULATI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface A2 : NSObject
+
+@end
---
+++ day1a/A2.m
@@ -0,0 +1,13 @@
+//
+//  A2.m
+//  day1a
+//
+//  Created by MEGHA GULATI on 9/9/13.
+//  Copyright (c) 2013 MEGHA GULATI. All rights reserved.
+//
+
+#import "A2.h"
+
+@implementation A2
+
+@end
